f5a4e961fa65ab14fae0df948bfad6575919bbc0
saruman9 <rum.274.4@gmail.com>
2019-11-09 23:50:42 +0300

    [i3blocks] Delete (replace by py3status)

--- dot_config/i3blocks/executable_music.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if pgrep mpd 1>/dev/null ; then
-    mpc | tr '\n' ' ' | grep -Po '.*(?= \[playing\])|paused' | tr -d '\n'
-elif pgrep pianobar 1>/dev/null ; then
-    cat $HOME/.config/pianobar/nowplaying
-else
-    echo ""
-fi
