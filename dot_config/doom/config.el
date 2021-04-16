bf5d0dcca1a3d41483142a1e3689a7e94dbe51a2
saruman9 <rum.274.4@gmail.com>
2021-04-16 22:00:09 +0300

    [emacs] Fix emacs-everywhere for markdown-mode

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -37,8 +37,10 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
-(push "Telegram" emacs-everywhere-markdown-windows)
-(push "firefox" emacs-everywhere-markdown-apps)
+(after! emacs-everywhere
+  (push "Telegram" emacs-everywhere-markdown-windows)
+  (push "firefox" emacs-everywhere-markdown-apps)
+  )
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
