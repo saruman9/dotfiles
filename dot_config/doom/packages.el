2ff19f0b2e0f1ff218f1e3c58bed7744f0fc0aa0
saruman9 <rum.274.4@gmail.com>
2021-04-20 23:35:47 +0300

    [emacs] Add org-roam-server package

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -11,6 +11,7 @@
 (package! visual-fill-column)
 (package! writegood-mode :disable t)
 (package! systemd)
+(package! org-roam-server)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
