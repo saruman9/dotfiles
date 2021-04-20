348670ccec7b4cf680f2334024ba0fbd7ad9ff68
saruman9 <rum.274.4@gmail.com>
2021-04-20 23:34:27 +0300

    [emacs] Add systemd package

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -10,6 +10,7 @@
 ;(package! some-package)
 (package! visual-fill-column)
 (package! writegood-mode :disable t)
+(package! systemd)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
