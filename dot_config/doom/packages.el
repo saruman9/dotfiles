875d5deff1cd26e5f7c131c96f69dd0a2c17aee1
saruman9 <rum.274.4@gmail.com>
2021-04-25 12:49:33 +0300

    [emacs] Use GhostText for editing in the Firefox (atomic-chrome package)

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -12,6 +12,7 @@
 (package! writegood-mode :disable t)
 (package! systemd)
 (package! org-roam-server)
+(package! atomic-chrome)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
