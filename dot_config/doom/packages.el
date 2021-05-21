277c3e50638046cc01b567e7a219a5d893d828fd
saruman9 <rum.274.4@gmail.com>
2021-05-21 11:51:43 +0300

    [emacs] Add protobuf-mode

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -14,6 +14,7 @@
 (package! org-roam-server)
 (package! atomic-chrome)
 (package! pkgbuild-mode)
+(package! protobuf-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
