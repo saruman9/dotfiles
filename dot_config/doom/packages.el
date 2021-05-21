1b6a5545c2981dfe309c6e0bbae250812f28b31b
saruman9 <rum.274.4@gmail.com>
2021-05-21 11:51:19 +0300

    [emacs] Add pkgbuild-mode

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -13,6 +13,7 @@
 (package! systemd)
 (package! org-roam-server)
 (package! atomic-chrome)
+(package! pkgbuild-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
